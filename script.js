fca4578df6d209f2d19f4634e9726ce1f46c5ec2
Ong Zhi Zheng <hewwo@ongzz.me>
2020-09-23 15:04:14 +0000

    add sitemanifest, favicon and png, goodnight world!!

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 let table = document.getElementById("bigTable");
 
-
 const url = "https://jp.rapidpg.com.my:10081/open/api/searcheta?stopName=smk%20chung%20ling%20(p)(kampung%20baru)";
 
 function appendToTable(data=[]){
@@ -8,10 +7,6 @@ function appendToTable(data=[]){
   // row number == number of buses
   for (i=0; i<data.length; i++){
     var route = data[i];
-    
-    route.etaList.forEach(bus => {
-      console.log(bus);
-    });
 
     var row = document.createElement('tr');
 
@@ -24,13 +19,10 @@ function appendToTable(data=[]){
     var cells = [routeNumCell, lingNumCell];
     var texts = [routeNumText, lingNumText];
 
-
     let etaList = route.etaList;    
-
-    if(etaList === undefined || etaList.length == 0){
+    if(etaList == undefined || etaList.length == 0){
       var etaCell = document.createElement('td');
       var etaText = document.createTextNode("whoops wait yixia");
-
       cells.push(etaCell);
       texts.push(etaText);
 
@@ -39,64 +31,15 @@ function appendToTable(data=[]){
       for(h=0; h<etaList.length; h++){
         var etaCell = document.createElement('td');
         var etaText = document.createTextNode(`${etaList[h].eta} mins`);
-
         cells.push(etaCell);
         texts.push(etaText);
       }
-    }   
-
-    console.log(cells);
-    console.log(texts);
+    }
 
     for(k=0; k<cells.length; k++){
       cells[k].appendChild(texts[k]);
       row.appendChild(cells[k]);
     }
-
-
-    // var importantStuff = ["routeNm", "lingNm", "etaList"];
-
-    // var row = document.createElement('tr');
-
-    // for(j=0; j<importantStuff.length; j++){
-    //   var cell = document.createElement('td');
-    //   var dataValue = data[i][importantStuff[j]];
-
-    //   //if it is the etaList,
-    //   if(Array.isArray(dataValue)){
-        
-
-    //   } else{
-    //     var text = document.createTextNode(dataValue);
-    //   }
-
-    //   cell.appendChild(text);
-    //   row.appendChild(cell);
-    // }
-
-
-
-
-    // number of cells == number of datas
-    // for(const [key, value] of Object.entries(data[i])){
-    //   var cell = document.createElement('td');
-    //   var innerText;
-
-    //   if(Array.isArray(value)){
-    //     if(value === undefined || value.length == 0){
-    //       innerText = document.createTextNode("whoops");
-    //     } else{
-    //       innerText = document.createTextNode(value[0].eta);
-    //     }
-    //   } else {
-    //     innerText = document.createTextNode(value);
-    //   }
-
-    //   cell.appendChild(innerText);
-    //   row.appendChild(cell);
-
-    //   // console.log(`${key} => ${value}`);
-    // }
 
     table.appendChild(row);
   }
@@ -106,7 +49,7 @@ function appendToTable(data=[]){
 fetch(url).then( 
   (response) => {
     if (response.status !== 200) {
-      console.log('Looks like there was a problem. Status Code: ' + response.status);
+      console.error(`whoopsie daisie (Status Code: ${response.status})`);
       return;
     }
     response.json().then(data => {
@@ -115,7 +58,7 @@ fetch(url).then(
     });
  }
 ).catch((err) => {
-    console.error('Fetch Error :-S', err);
+    console.error(`Fetch Error :-S ${err}`);
 });
 
 /////::::::::::SAMPLE JSON:::::::::://///
