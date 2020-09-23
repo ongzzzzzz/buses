ff255db29f24125d887eec6457ce6545d9b02dd4
Ong Zhi Zheng <hewwo@ongzz.me>
2020-09-23 11:38:25 +0000

    dynamic table!!!

--- script.js
+++ script.js
@@ -5,21 +5,99 @@ const url = "https://jp.rapidpg.com.my:10081/open/api/searcheta?stopName=smk%20c
 
 function appendToTable(data=[]){
 
-  // row number == number of stops
+  // row number == number of buses
   for (i=0; i<data.length; i++){
-    console.log(data[i].lingNm);
+    var route = data[i];
+    
+    route.etaList.forEach(bus => {
+      console.log(bus);
+    });
+
     var row = document.createElement('tr');
 
-    // number of cells == number of datas
-    for(const [key, value] of Object.entries(data[i])){
-      var cell = document.createElement('td');
-      var innerText = document.createTextNode(value);
+    var routeNumCell = document.createElement('td');
+    var routeNumText = document.createTextNode(route.routeNm);
 
-      cell.appendChild(innerText);
-      row.appendChild(cell);
+    var lingNumCell = document.createElement('td');
+    var lingNumText = document.createTextNode(route.lingNm);
 
-      // console.log(`${key} => ${value}`);
+    var cells = [routeNumCell, lingNumCell];
+    var texts = [routeNumText, lingNumText];
+
+
+    let etaList = route.etaList;    
+
+    if(etaList === undefined || etaList.length == 0){
+      var etaCell = document.createElement('td');
+      var etaText = document.createTextNode("whoops wait yixia");
+
+      cells.push(etaCell);
+      texts.push(etaText);
+
+    } else {
+
+      for(h=0; h<etaList.length; h++){
+        var etaCell = document.createElement('td');
+        var etaText = document.createTextNode(`${etaList[h].eta} mins`);
+
+        cells.push(etaCell);
+        texts.push(etaText);
+      }
+    }   
+
+    console.log(cells);
+    console.log(texts);
+
+    for(k=0; k<cells.length; k++){
+      cells[k].appendChild(texts[k]);
+      row.appendChild(cells[k]);
     }
+
+
+    // var importantStuff = ["routeNm", "lingNm", "etaList"];
+
+    // var row = document.createElement('tr');
+
+    // for(j=0; j<importantStuff.length; j++){
+    //   var cell = document.createElement('td');
+    //   var dataValue = data[i][importantStuff[j]];
+
+    //   //if it is the etaList,
+    //   if(Array.isArray(dataValue)){
+        
+
+    //   } else{
+    //     var text = document.createTextNode(dataValue);
+    //   }
+
+    //   cell.appendChild(text);
+    //   row.appendChild(cell);
+    // }
+
+
+
+
+    // number of cells == number of datas
+    // for(const [key, value] of Object.entries(data[i])){
+    //   var cell = document.createElement('td');
+    //   var innerText;
+
+    //   if(Array.isArray(value)){
+    //     if(value === undefined || value.length == 0){
+    //       innerText = document.createTextNode("whoops");
+    //     } else{
+    //       innerText = document.createTextNode(value[0].eta);
+    //     }
+    //   } else {
+    //     innerText = document.createTextNode(value);
+    //   }
+
+    //   cell.appendChild(innerText);
+    //   row.appendChild(cell);
+
+    //   // console.log(`${key} => ${value}`);
+    // }
+
     table.appendChild(row);
   }
   return;
@@ -47,18 +125,6 @@ fetch(url).then(
 //   "message": "Nearest Stops",
 //   "body": [
 //     {
-//       "routeId": "30000007",
-//       "routeNm": "203",
-//       "lingId": "1000000427",
-//       "lingNm": "JETI - AIR ITAM",
-//       "dirCd": "0",
-//       "stopId": 12000066,
-//       "stopNm": "SMK Chung Ling (P)(Kampung baru)",
-//       "stopSeq": 21,
-//       "stopCode": "I0Q0066",
-//       "etaList": null
-//     },
-//     {
 //       "routeId": "30000024",
 //       "routeNm": "502",
 //       "lingId": "1000000484",
@@ -68,7 +134,17 @@ fetch(url).then(
 //       "stopNm": "SMK Chung Ling (P)(Kampung baru)",
 //       "stopSeq": 22,
 //       "stopCode": "I0Q0066",
-//       "etaList": null
+//       "etaList": [
+//         {
+//           "stopId": 12000066,
+//           "vehicleId": "PLA2935",
+//           "routeId": "30000024",
+//           "lineId": "1000000484",
+//           "vehicleLat": "5.416342",
+//           "vehicleLon": "100.330791",
+//           "eta": 23
+//         }
+//       ]
 //     },
 //     {
 //       "routeId": "30000016",
@@ -80,7 +156,7 @@ fetch(url).then(
 //       "stopNm": "SMK Chung Ling (P)(Kampung baru)",
 //       "stopSeq": 5,
 //       "stopCode": "I0Q0066",
-//       "etaList": null
+//       "etaList": []
 //     },
 //     {
 //       "routeId": "30000074",
@@ -92,7 +168,19 @@ fetch(url).then(
 //       "stopNm": "SMK Chung Ling (P)(Kampung baru)",
 //       "stopSeq": 27,
 //       "stopCode": "I0Q0066",
-//       "etaList": null
+//       "etaList": []
+//     },
+//     {
+//       "routeId": "30000007",
+//       "routeNm": "203",
+//       "lingId": "1000000427",
+//       "lingNm": "JETI - AIR ITAM",
+//       "dirCd": "0",
+//       "stopId": 12000066,
+//       "stopNm": "SMK Chung Ling (P)(Kampung baru)",
+//       "stopSeq": 21,
+//       "stopCode": "I0Q0066",
+//       "etaList": []
 //     }
 //   ]
 // }
