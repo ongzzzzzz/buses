3b2b130aeec01c84ef38346185e8d40843e0ec99
Ong Zhi Zheng <hewwo@ongzz.me>
2020-10-14 11:15:20 +0000

    add dark background

--- script.js
+++ script.js
@@ -23,16 +23,6 @@ function checkTime(i) {
   return i<10 ? "0"+i : i;
 }
 
-async function request(url){
-  let response = await fetch(url);
-  let json = await response.json();
-  let arr = [];
-  json.forEach(stop => {
-    arr.push(stop.poiName);
-  });
-  return arr;
-}
-
 function appendToTable(data=[]){
   //clear table first
   tableBody.innerHTML = "";
@@ -78,7 +68,7 @@ function appendToTable(data=[]){
 }
 
 function updateWeather(data=[]){
-  let weatherIcon = document.getElementById("weather");
+  let weatherIcon = document.getElementById("weatherIcon");
   let weatherText = document.getElementById("weatherText");
 
   weatherIcon.src = `https://openweathermap.org/img/w/${data[0].icon}.png`;
@@ -140,86 +130,38 @@ function autocomplete(inp, arr) {
   /*the autocomplete function takes two arguments,
   the text field element and an array of possible autocompleted values:*/
   var currentFocus;
-  /*execute a function when someone writes in the text field:*/
+  
   inp.addEventListener("input", function(e) {
     var a, b, i, val = this.value;
-    /*close any already open lists of autocompleted values*/
     closeAllLists();
     if (!val) { return false;}
     currentFocus = -1;
-    /*create a DIV element that will contain the items (values):*/
+    
     a = document.createElement("DIV");
     a.setAttribute("id", this.id + "autocomplete-list");
     a.setAttribute("class", "autocomplete-items");
-    /*append the DIV element as a child of the autocomplete container:*/
+
+    /*append div:*/
     this.parentNode.appendChild(a);
-    /*for each item in the array...*/
+
     for (i = 0; i < arr.length; i++) {
-      /*create a DIV element for each matching element:*/
+      /*create a DIV element for each thing*/
       b = document.createElement("DIV");
-      /*make the matching letters bold:*/
       b.innerHTML += arr[i];
-      /*insert a input field that will hold the current array item's value:*/
+      /*insert a input field with the current array item's value:*/
       b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
       b.classList.add("stop");
-      /*execute a function when someone clicks on the item value (DIV element):*/
+      /* when someone clicks on the item value*/
       b.addEventListener("click", function(e) {
         /*insert the value for the autocomplete text field:*/
         inp.value = this.getElementsByTagName("input")[0].value;
-        /*close the list of autocompleted values, (or any other open lists of autocompleted values:*/
         closeAllLists();
       });
       a.appendChild(b);
     }   
   });
 
-
-  /*execute a function presses a key on the keyboard:*/
-  // inp.addEventListener("keydown", function(e) {
-  //     var x = document.getElementById(this.id + "autocomplete-list");
-  //     if (x) x = x.getElementsByTagName("div");
-  //     if (e.keyCode == 40) {
-  //       /*If the arrow DOWN key is pressed,
-  //       increase the currentFocus variable:*/
-  //       currentFocus++;
-  //       /*and and make the current item more visible:*/
-  //       addActive(x);
-  //     } else if (e.keyCode == 38) { //up
-  //       /*If the arrow UP key is pressed,
-  //       decrease the currentFocus variable:*/
-  //       currentFocus--;
-  //       /*and and make the current item more visible:*/
-  //       addActive(x);
-  //     } else if (e.keyCode == 13) {
-  //       /*If the ENTER key is pressed, prevent the form from being submitted,*/
-  //       e.preventDefault();
-  //       if (currentFocus > -1) {
-  //         /*and simulate a click on the "active" item:*/
-  //         if (x) x[currentFocus].click();
-  //       }
-  //     }
-  // });
-  // function addActive(x) {
-  //   /*a function to classify an item as "active":*/
-  //   if (!x) return false;
-  //   /*start by removing the "active" class on all items:*/
-  //   removeActive(x);
-  //   if (currentFocus >= x.length) currentFocus = 0;
-  //   if (currentFocus < 0) currentFocus = (x.length - 1);
-  //   /*add class "autocomplete-active":*/
-  //   x[currentFocus].classList.add("autocomplete-active");
-  // }
-  // function removeActive(x) {
-  //   /*a function to remove the "active" class from all autocomplete items:*/
-  //   for (var i = 0; i < x.length; i++) {
-  //     x[i].classList.remove("autocomplete-active");
-  //   }
-  // }
-
-
   function closeAllLists(elmnt) {
-    /*close all autocomplete lists in the document,
-    except the one passed as an argument:*/
     var x = document.getElementsByClassName("autocomplete-items");
     for (var i = 0; i < x.length; i++) {
       if (elmnt != x[i] && elmnt != inp) {
@@ -227,15 +169,13 @@ function autocomplete(inp, arr) {
       }
     }
   }
-  /*execute a function when someone clicks in the document:*/
+  
   document.addEventListener("click", function (e) {
       closeAllLists(e.target);
   });
 }
 
 /* 
-  TODO: add other destination search
-  
   TODO: add map (Google map embed)
   TODO: deploy
   
